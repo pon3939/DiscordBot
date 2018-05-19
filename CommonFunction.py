9cbeabdfc8e3f49286c56cdf751b3cd3d8819065
Unknown <ryuu3939@outlook.jp>
2018-05-19 23:38:42 +0900

    定数名変更

--- CommonFunction.py
+++ CommonFunction.py
@@ -109,9 +109,9 @@ class CommonFunction:
         :return: 選択した通知メッセージ
         """
         try:
-            f = open(self.ini.get(CommonConstants.INI_SECTION_GENERAL, CommonConstants.INI_OPTION_EXEC_DIR) + "Json/Message.json", "r", CommonConstants.FILE_ENCODING)
+            f = open(self.ini.get(CommonConstants.INI_SECTION_GENERAL, CommonConstants.INI_OPTION_EXEC_DIR) + "Json/" + JSON_NAME_MESSAGE + ".json", "r", CommonConstants.FILE_ENCODING)
             json_data = load(f)
-            messageList = json_data[CommonConstants.JSON_MESSAGE_LONELY]
+            messageList = json_data[CommonConstants.JSON_KEY_LONELY]
             message = messageList[randint(0, len(messageList) - 1)] # ランダムにメッセージを選択
             return message.replace(CommonConstants.LONELY_MESSAGE_NAME, name).replace(CommonConstants.LONELY_MESSAGE_CHANNEL, channel) # ユーザ名、チャンネル名を置換
         except Exception as e:
@@ -128,9 +128,9 @@ class CommonFunction:
         """
         try:
             rtn = ""
-            with open(self.ini.get(CommonConstants.INI_SECTION_GENERAL, CommonConstants.INI_OPTION_EXEC_DIR) + "Json/Message.json", "r", CommonConstants.FILE_ENCODING) as f:
+            with open(self.ini.get(CommonConstants.INI_SECTION_GENERAL, CommonConstants.INI_OPTION_EXEC_DIR) + "Json/" + JSON_NAME_MESSAGE + ".json", "r", CommonConstants.FILE_ENCODING) as f:
                 json_data = load(f)
-            messageList = json_data[CommonConstants.JSON_MESSAGE_LONELY]
+            messageList = json_data[CommonConstants.JSON_KEY_LONELY]
 
             i = 0
             for message in messageList:
@@ -159,15 +159,15 @@ class CommonFunction:
             elif message.find(CommonConstants.LONELY_MESSAGE_CHANNEL) == -1:
                 return "テンプレートには" + CommonConstants.LONELY_MESSAGE_CHANNEL + "が含まれる必要があります"
 
-            with open(self.ini.get(CommonConstants.INI_SECTION_GENERAL, CommonConstants.INI_OPTION_EXEC_DIR) + "Json/Message.json", "r", CommonConstants.FILE_ENCODING) as fIn:
+            with open(self.ini.get(CommonConstants.INI_SECTION_GENERAL, CommonConstants.INI_OPTION_EXEC_DIR) + "Json/" + JSON_NAME_MESSAGE + ".json", "r", CommonConstants.FILE_ENCODING) as fIn:
                 json_data = load(fIn)
-            messageList = json_data[CommonConstants.JSON_MESSAGE_LONELY]
+            messageList = json_data[CommonConstants.JSON_KEY_LONELY]
             if message in messageList:
                 return "入力されたテンプレートは既に存在します"
 
             messageList.append(message)
-            json_data[CommonConstants.JSON_MESSAGE_LONELY] = messageList
-            with open(self.ini.get(CommonConstants.INI_SECTION_GENERAL, CommonConstants.INI_OPTION_EXEC_DIR) + "Json/Message.json", "w", CommonConstants.FILE_ENCODING) as fOut:
+            json_data[CommonConstants.JSON_KEY_LONELY] = messageList
+            with open(self.ini.get(CommonConstants.INI_SECTION_GENERAL, CommonConstants.INI_OPTION_EXEC_DIR) + "Json/" + JSON_NAME_MESSAGE + ".json", "w", CommonConstants.FILE_ENCODING) as fOut:
                 dump(json_data, fOut) # JSONファイル更新
             return "テンプレート\"" + message + "\"を追加しました"
         except Exception as e:
@@ -191,16 +191,16 @@ class CommonFunction:
             if index < 0:
                 return "0以上の数値を指定してください"
 
-            with open(self.ini.get(CommonConstants.INI_SECTION_GENERAL, CommonConstants.INI_OPTION_EXEC_DIR) + "Json/Message.json", "r", CommonConstants.FILE_ENCODING) as fIn:
+            with open(self.ini.get(CommonConstants.INI_SECTION_GENERAL, CommonConstants.INI_OPTION_EXEC_DIR) + "Json/" + JSON_NAME_MESSAGE + ".json", "r", CommonConstants.FILE_ENCODING) as fIn:
                 json_data = load(fIn)
-            messageList = json_data[CommonConstants.JSON_MESSAGE_LONELY]
+            messageList = json_data[CommonConstants.JSON_KEY_LONELY]
             if len(messageList) <= index:
                 return "入力されたインデックスのテンプレートは存在しません"
 
             delMessage = messageList[index] # チャットに流すためにテンプレートを取得
             messageList.pop(index)
-            json_data[CommonConstants.JSON_MESSAGE_LONELY] = messageList
-            with open(self.ini.get(CommonConstants.INI_SECTION_GENERAL, CommonConstants.INI_OPTION_EXEC_DIR) + "Json/Message.json", "w", CommonConstants.FILE_ENCODING) as fOut:
+            json_data[CommonConstants.JSON_KEY_LONELY] = messageList
+            with open(self.ini.get(CommonConstants.INI_SECTION_GENERAL, CommonConstants.INI_OPTION_EXEC_DIR) + "Json/" + JSON_NAME_MESSAGE + ".json", "w", CommonConstants.FILE_ENCODING) as fOut:
                 dump(json_data, fOut) # JSONファイル更新
             return "テンプレート\"" + delMessage +  "\"を削除しました"
         except Exception as e:
@@ -218,7 +218,7 @@ class CommonFunction:
         try:
             response = get(CommonConstants.README_URL) # githubのreadme取得APIをコール
             responseJson = response.json()
-            readme = b64decode(responseJson[CommonConstants.JSON_README_CONTENT]).decode("utf_8") #結果をbase64でデコードして、さらにutf8でデコード
+            readme = b64decode(responseJson[CommonConstants.JSON_KEY_CONTENT]).decode("utf_8") #結果をbase64でデコードして、さらにutf8でデコード
 
             rtn = []
             for line in readme.split("\n"):
@@ -282,16 +282,16 @@ class CommonFunction:
         try:
             rtn = ""
             # Jsonファイル取得
-            with open(self.ini.get(CommonConstants.INI_SECTION_GENERAL, CommonConstants.INI_OPTION_EXEC_DIR) + "Json/Task.json", "r", CommonConstants.FILE_ENCODING) as fIn:
+            with open(self.ini.get(CommonConstants.INI_SECTION_GENERAL, CommonConstants.INI_OPTION_EXEC_DIR) + "Json/" + JSON_NAME_TASK + ".json", "r", CommonConstants.FILE_ENCODING) as fIn:
                 json_data = load(fIn)
 
-            for channel in json_data[CommonConstants.JSON_TASK_CHANNEL]:
-                if channel[CommonConstants.JSON_TASK_CHANNEL_ID] == channelID:
+            for channel in json_data[CommonConstants.JSON_KEY_CHANNEL]:
+                if channel[CommonConstants.JSON_KEY_CHANNEL_ID] == channelID:
                     i = 0
-                    for task in channel[CommonConstants.JSON_TASK_TASK]:
+                    for task in channel[CommonConstants.JSON_KEY_TASK]:
                         if rtn != "":
                             rtn += "\n" # 2件目以降は改行
-                        rtn += str(i) + " : " + task[CommonConstants.JSON_TASK_TIME] + " " + task[CommonConstants.JSON_TASK_MESSAGE]
+                        rtn += str(i) + " : " + task[CommonConstants.JSON_KEY_TIME] + " " + task[CommonConstants.JSON_KEY_MESSAGE]
                         i += 1
                     break
 
@@ -326,29 +326,29 @@ class CommonFunction:
             # タスクを作成
             taskMessage = " ".join(splitList[2:])
             task = {}
-            task[CommonConstants.JSON_TASK_TIME] = time.strftime("%Y-%m-%d %H:%M")
-            task[CommonConstants.JSON_TASK_MESSAGE] = taskMessage
+            task[CommonConstants.JSON_KEY_TIME] = time.strftime("%Y-%m-%d %H:%M")
+            task[CommonConstants.JSON_KEY_MESSAGE] = taskMessage
 
             # Jsonファイル取得
-            with open(self.ini.get(CommonConstants.INI_SECTION_GENERAL, CommonConstants.INI_OPTION_EXEC_DIR) + "Json/Task.json", "r", CommonConstants.FILE_ENCODING) as fIn:
+            with open(self.ini.get(CommonConstants.INI_SECTION_GENERAL, CommonConstants.INI_OPTION_EXEC_DIR) + "Json/" + JSON_NAME_TASK + ".json", "r", CommonConstants.FILE_ENCODING) as fIn:
                 json_data = load(fIn)
 
             # チャンネルIDで検索して追加
             exists = False
-            for i in range(len(json_data[CommonConstants.JSON_TASK_CHANNEL])):
-                if json_data[CommonConstants.JSON_TASK_CHANNEL][i][CommonConstants.JSON_TASK_CHANNEL_ID] == channelID:
-                    json_data[CommonConstants.JSON_TASK_CHANNEL][i][CommonConstants.JSON_TASK_TASK].append(task)
+            for i in range(len(json_data[CommonConstants.JSON_KEY_CHANNEL])):
+                if json_data[CommonConstants.JSON_KEY_CHANNEL][i][CommonConstants.JSON_KEY_CHANNEL_ID] == channelID:
+                    json_data[CommonConstants.JSON_KEY_CHANNEL][i][CommonConstants.JSON_KEY_TASK].append(task)
                     exists = True
                     break
 
             if not exists:
                 # なければ作成
                 channel = {}
-                channel[CommonConstants.JSON_TASK_CHANNEL_ID] = channelID
-                channel[CommonConstants.JSON_TASK_TASK] = [task]
-                json_data[CommonConstants.JSON_TASK_CHANNEL].append(channel)
+                channel[CommonConstants.JSON_KEY_CHANNEL_ID] = channelID
+                channel[CommonConstants.JSON_KEY_TASK] = [task]
+                json_data[CommonConstants.JSON_KEY_CHANNEL].append(channel)
 
-            with open(self.ini.get(CommonConstants.INI_SECTION_GENERAL, CommonConstants.INI_OPTION_EXEC_DIR) + "Json/Task.json", "w", CommonConstants.FILE_ENCODING) as fOut:
+            with open(self.ini.get(CommonConstants.INI_SECTION_GENERAL, CommonConstants.INI_OPTION_EXEC_DIR) + "Json/" + JSON_NAME_TASK + ".json", "w", CommonConstants.FILE_ENCODING) as fOut:
                 dump(json_data, fOut) # JSONファイル更新
             return time.strftime("%Y-%m-%d %H:%M") + "に" + taskMessage + "を追加しました。"
         except Exception as e:
@@ -373,25 +373,25 @@ class CommonFunction:
             if index < 0:
                 return "0以上の数値を指定してください"
 
-            with open(self.ini.get(CommonConstants.INI_SECTION_GENERAL, CommonConstants.INI_OPTION_EXEC_DIR) + "Json/Task.json", "r", CommonConstants.FILE_ENCODING) as fIn:
+            with open(self.ini.get(CommonConstants.INI_SECTION_GENERAL, CommonConstants.INI_OPTION_EXEC_DIR) + "Json/" + JSON_NAME_TASK + ".json", "r", CommonConstants.FILE_ENCODING) as fIn:
                 json_data = load(fIn)
 
             # チャンネルIDで検索して削除
             exists = False
-            for i in range(len(json_data[CommonConstants.JSON_TASK_CHANNEL])):
-                if json_data[CommonConstants.JSON_TASK_CHANNEL][i][CommonConstants.JSON_TASK_CHANNEL_ID] == channelID:
-                    if index < len(json_data[CommonConstants.JSON_TASK_CHANNEL][i][CommonConstants.JSON_TASK_TASK]):
+            for i in range(len(json_data[CommonConstants.JSON_KEY_CHANNEL])):
+                if json_data[CommonConstants.JSON_KEY_CHANNEL][i][CommonConstants.JSON_KEY_CHANNEL_ID] == channelID:
+                    if index < len(json_data[CommonConstants.JSON_KEY_CHANNEL][i][CommonConstants.JSON_KEY_TASK]):
                         exists = True
-                        delTask = json_data[CommonConstants.JSON_TASK_CHANNEL][i][CommonConstants.JSON_TASK_TASK][index] # チャットに流すためにタスクを取得
-                        json_data[CommonConstants.JSON_TASK_CHANNEL][i][CommonConstants.JSON_TASK_TASK].pop(index)
+                        delTask = json_data[CommonConstants.JSON_KEY_CHANNEL][i][CommonConstants.JSON_KEY_TASK][index] # チャットに流すためにタスクを取得
+                        json_data[CommonConstants.JSON_KEY_CHANNEL][i][CommonConstants.JSON_KEY_TASK].pop(index)
                     break
 
             if not exists:
                 return "入力されたインデックスのタスクは存在しません"
 
-            with open(self.ini.get(CommonConstants.INI_SECTION_GENERAL, CommonConstants.INI_OPTION_EXEC_DIR) + "Json/Task.json", "w", CommonConstants.FILE_ENCODING) as fOut:
+            with open(self.ini.get(CommonConstants.INI_SECTION_GENERAL, CommonConstants.INI_OPTION_EXEC_DIR) + "Json/" + JSON_NAME_TASK + ".json", "w", CommonConstants.FILE_ENCODING) as fOut:
                 dump(json_data, fOut) # JSONファイル更新
-            return "タスク\"" + delTask[CommonConstants.JSON_TASK_TIME] + " " + delTask[CommonConstants.JSON_TASK_MESSAGE] +  "\"を削除しました"
+            return "タスク\"" + delTask[CommonConstants.JSON_KEY_TIME] + " " + delTask[CommonConstants.JSON_KEY_MESSAGE] +  "\"を削除しました"
         except Exception as e:
             self.logger.error("addTaskList:例外発生")
             self.logger.error(e)
@@ -418,21 +418,21 @@ class CommonFunction:
         """
         try:
             self.logger.info("remindTask開始")
-            with open(self.ini.get(CommonConstants.INI_SECTION_GENERAL, CommonConstants.INI_OPTION_EXEC_DIR) + "Json/Task.json", "r", CommonConstants.FILE_ENCODING) as fIn:
+            with open(self.ini.get(CommonConstants.INI_SECTION_GENERAL, CommonConstants.INI_OPTION_EXEC_DIR) + "Json/" + JSON_NAME_TASK + ".json", "r", CommonConstants.FILE_ENCODING) as fIn:
                 json_data = load(fIn)
 
-            for i in range(len(json_data[CommonConstants.JSON_TASK_CHANNEL])):
-                channelID = json_data[CommonConstants.JSON_TASK_CHANNEL][i][CommonConstants.JSON_TASK_CHANNEL_ID]
-                for j in range(len(json_data[CommonConstants.JSON_TASK_CHANNEL][i][CommonConstants.JSON_TASK_TASK])):
-                    taskTime = json_data[CommonConstants.JSON_TASK_CHANNEL][i][CommonConstants.JSON_TASK_TASK][j][CommonConstants.JSON_TASK_TIME]
-                    taskMessage = json_data[CommonConstants.JSON_TASK_CHANNEL][i][CommonConstants.JSON_TASK_TASK][j][CommonConstants.JSON_TASK_MESSAGE]
+            for i in range(len(json_data[CommonConstants.JSON_KEY_CHANNEL])):
+                channelID = json_data[CommonConstants.JSON_KEY_CHANNEL][i][CommonConstants.JSON_KEY_CHANNEL_ID]
+                for j in range(len(json_data[CommonConstants.JSON_KEY_CHANNEL][i][CommonConstants.JSON_KEY_TASK])):
+                    taskTime = json_data[CommonConstants.JSON_KEY_CHANNEL][i][CommonConstants.JSON_KEY_TASK][j][CommonConstants.JSON_KEY_TIME]
+                    taskMessage = json_data[CommonConstants.JSON_KEY_CHANNEL][i][CommonConstants.JSON_KEY_TASK][j][CommonConstants.JSON_KEY_MESSAGE]
                     if self.strToDatetime(taskTime) < datetime.now():
                         # 時間を過ぎているものはリマインドして削除
                         message = "リマインド : " + taskTime + " " + taskMessage
                         await self.client.send_message(self.client.get_channel(channelID), message)
-                        json_data[CommonConstants.JSON_TASK_CHANNEL][i][CommonConstants.JSON_TASK_TASK].pop(j)
+                        json_data[CommonConstants.JSON_KEY_CHANNEL][i][CommonConstants.JSON_KEY_TASK].pop(j)
 
-            with open(self.ini.get(CommonConstants.INI_SECTION_GENERAL, CommonConstants.INI_OPTION_EXEC_DIR) + "Json/Task.json", "w", CommonConstants.FILE_ENCODING) as fOut:
+            with open(self.ini.get(CommonConstants.INI_SECTION_GENERAL, CommonConstants.INI_OPTION_EXEC_DIR) + "Json/" + JSON_NAME_TASK + ".json", "w", CommonConstants.FILE_ENCODING) as fOut:
                 dump(json_data, fOut) # JSONファイル更新
         except Exception as e:
             self.logger.error("remindTask:例外発生")
